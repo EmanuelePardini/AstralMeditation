80e52352abfb53ef752f71c368439759b9acff03
Emanuele Pardini <66436200+EmanuelePardini@users.noreply.github.com>
2023-08-11 22:50:53 +0100

    Create RoomData.js

---
+++ utils/RoomData.js
@@ -0,0 +1,40 @@
+import hanamimp4 from '../media/hanami.mp4'
+import hanamimp3 from '../media/hanami.mp3'
+import rainmp4 from '../media/rain.mp4'
+import rainmp3 from '../media/rain.mp3'
+import gargantuabluemp4 from '../media/gargantuablue.mp4'
+import gargantuabluemp3 from '../media/gargantuablue.mp3'
+import fairyfallsmp4 from '../media/fairyfalls.mp4'
+import fairyfallsmp3 from '../media/fairyfalls.mp3'
+import interstellarmp4 from '../media/interstellar.mp4'
+import interstellarmp3 from '../media/interstellar.mp3'
+import homemp4 from '../media/home.mp4'
+
+
+
+export const roomData = {
+  hanami: {
+    video: hanamimp4,
+    audio: hanamimp3,
+  },
+  rain: {
+    video: rainmp4,
+    audio: rainmp3,
+  },
+    gargantuablue: {
+      video: gargantuabluemp4,
+      audio: gargantuabluemp3,
+    },
+    fairyfalls: {
+      video: fairyfallsmp4,
+      audio: fairyfallsmp3,
+    },
+    interstellar: {
+      video: interstellarmp4,
+      audio: interstellarmp3,
+    },
+    home: {
+      video: homemp4,
+    },
+
+  };
